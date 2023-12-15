41ea5899c4a8c1bd5101f8eae1de55382711f5d6
Baebou <152212003+Baebou@users.noreply.github.com>
2023-12-15 14:23:24 -0300

    Atualizar o WackyRaces.cs

--- pooRacingGame/WackyRaces.cs
+++ pooRacingGame/WackyRaces.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+using System.Threading;// ao adicionar esse Threading ao sistema fara com que funcione o Thread.Sleep
 
 namespace pooRacingGame
 {
